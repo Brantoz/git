fcae8ec0cb6bc1c0775168ac434a360fa6a6169d
Brantoz <brantohmwaz@gmail.com>
2024-10-25 11:17:12 +0300

    git commit -master

--- version1.js
+++ version1.js
@@ -1,3 +1,3 @@
-const fruits = ['apples', 'bananas']
+const fruits = ['apples', 'bananas', 'mango']
 console.log(fruits[0]);
 console.log('finished tasks');
---
+++ version2.js
@@ -0,0 +1,6 @@
+const person = {
+    name : "john",
+    age : 20,
+    gender : male
+}
+console.log(person);
